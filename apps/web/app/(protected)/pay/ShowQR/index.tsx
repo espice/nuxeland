41d99e9adf3ed05b44b0716447a99aca2ce4514e
Arnav Bansal <arnavbansal101@gmail.com>
2023-10-30 11:05:28 +0530

    fix stripe key issue

--- apps/web/app/(protected)/pay/ShowQR/index.tsx
+++ apps/web/app/(protected)/pay/ShowQR/index.tsx
@@ -2,9 +2,12 @@
 
 import useOnClickOutside, { Popup } from "@/components/Popup";
 import QRCodeIcon from "@/public/qrcode.svg";
+import QRCode from "react-qr-code";
+import { useUser } from "@/utils/hooks/useUser";
 import { useState, useRef } from "react";
 
 export default function ShowQRButton() {
+  const user = useUser();
   const showQRRef = useRef();
   const [showQRModal, setShowQRModal] = useState(false);
 
@@ -20,8 +23,14 @@ export default function ShowQRButton() {
       }}
     >
       <QRCodeIcon size={30} />
-      <Popup className="" popupState={showQRModal} ref={showQRRef}>
-        Your QR Code
+      <Popup className="flex flex-col" popupState={showQRModal} ref={showQRRef}>
+        <p className="text-[#3F3F3F] font-bold text-[22px]">Your QR Code</p>
+        <p className="font-[600] text-[#5B5B5B] text-[17px] leading-[-30%]">
+          Show this QR Code to others to receive payments
+        </p>
+        <div className="mt-[20px] ml-auto mr-auto">
+          <QRCode value={user!.id} fgColor={"#3F3F3F"} />
+        </div>
       </Popup>
     </div>
   );
--- packages/backend/src/client/stripe.ts
+++ packages/backend/src/client/stripe.ts
@@ -1,6 +1,6 @@
 import { env } from "../utils/env";
 import Stripe from "stripe";
 
-const stripe = new Stripe(env("STRIPE_SECRET_KEY"));
+const stripe = new Stripe(env("STRIPE_SECRET_KEY", "hello"));
 
 export { stripe };
