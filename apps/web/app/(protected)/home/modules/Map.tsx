1d37237f0f5deea5e80123a3b84bdadcb34b3216
Yash Tripathi <yashtri22@gmail.com>
2023-11-04 08:48:19 +0530

    top 10 bugs

--- apps/web/app/(protected)/home/modules/Map.tsx
+++ apps/web/app/(protected)/home/modules/Map.tsx
@@ -61,6 +61,10 @@ const Map = ({ total }: { total: number }) => {
             alignItems: "center",
             overflow: "hidden",
           }}
+          
+          onClick={() => {
+            setExpand(false);
+          }}
         >
           <svg
             style={{
@@ -82,9 +86,6 @@ const Map = ({ total }: { total: number }) => {
               fill-rule="evenodd"
               d="M11.782 4.032a.575.575 0 1 0-.813-.814L7.5 6.687L4.032 3.218a.575.575 0 0 0-.814.814L6.687 7.5l-3.469 3.468a.575.575 0 0 0 .814.814L7.5 8.313l3.469 3.469a.575.575 0 0 0 .813-.814L8.313 7.5l3.469-3.468Z"
               clip-rule="evenodd"
-              onClick={() => {
-                setExpand(false);
-              }}
             />
           </svg>
           <div className={styles.overlay}></div>
@@ -492,12 +493,16 @@ const Map = ({ total }: { total: number }) => {
           </svg>
         </div>
         {total - 1 <= 0 ? (
-          <>No other humans are currently exploring nuxEland!</>
+          <div style={{
+            fontSize:"13px"
+          }}>No other humans are currently exploring nuxEland!</div>
         ) : (
-          <>
+          <div style={{
+            fontSize:"14px"
+          }}>
             {total - 1} other {total - 1 == 1 ? "human" : "humans"}{" "}
             {total - 1 == 1 ? "is" : "are"} currently exploring nuxEland!
-          </>
+          </div>
         )}
       </div>
     </div>
